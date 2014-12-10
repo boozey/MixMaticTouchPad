233ef0766ed213334561347cf4675987e9778541
boozey <lastplace@hotmail.com>
2014-12-09 21:01:45 -0700

    Working on beat detection algorithm

--- app/src/main/java/com/nakedape/mixmaticlooppad/AudioProcessor.java
+++ app/src/main/java/com/nakedape/mixmaticlooppad/AudioProcessor.java
@@ -29,8 +29,9 @@ public class AudioProcessor {
 
     //Beat detection methods
     public ArrayList<BeatInfo> detectBeats() {
-        //Array to hold detected beats
+        //Arrays to hold detected beats
         ArrayList<BeatInfo> beats = new ArrayList<BeatInfo>();
+        ArrayList<BeatInfo> finalBeatList = new ArrayList<BeatInfo>();
         InputStream wavStream = null;
         File sampleFile = new File(wavPath); // File pointer to the current wav sample
         // If the sample file exists, try to generate the waveform
@@ -46,12 +47,13 @@ public class AudioProcessor {
                 ByteBuffer bb = ByteBuffer.wrap(lenInt).order(ByteOrder.LITTLE_ENDIAN);
                 length = bb.getInt();
                 //Initial beats size is large enough to hold beats for 240 bpm
-                beats = new ArrayList<BeatInfo>((int)(length / 44100 / 4) * 4);
+                beats = new ArrayList<BeatInfo>((int)(length / 44100 / 4) * 4 * 3);
 
                 float[] E = new float[43]; // Energy averages for last second
                 float e; // Instant sound energy
                 float avgE; // Average sound energy over the interval
-                double C = 1.3; // Beat detection constant
+                double V; // Variance
+                double C = 1.4; // Beat detection constant
                 int count = 0; // Number of calculations done;
                 int bufferSize = 2048;
                 byte[] bytesBuffer = new byte[bufferSize * 2];
@@ -75,13 +77,46 @@ public class AudioProcessor {
                         e += a[i] * a[i] + b[i] * b[i];
                     }
                     count++;
-                    // Calculate average sound energy over the interval once at least 43
-                    if (count >= 10) {
+                    if (count == 43) {
+                        // Once 43 samples have been obtained, check the data for beats
                         avgE = 0;
                         for (int i = 0; i < E.length; i++) {
                             avgE += E[i];
                         }
                         avgE /= E.length;
+                        // Calculate C value
+                        /*
+                        V = 0;
+                        for (int i = 0; i < E.length; i++){
+                            V += E[i] - avgE;
+                        }
+                        V /= E.length;
+                        C = -0.0025714 * V + 1.5142875;
+                        */
+                        for (int i = 0; i < E.length; i++){
+                            // Compare instant and average energy to detect a beat
+                            if (E[i] > C * avgE) {
+                                beats.add(new BeatInfo((double)(bufferSize * 2 * i) / 44100 / 4, 1));
+                                Log.d(LOG_TAG, "Beat detected at " + String.valueOf((double)position / 44100 / 4));
+                            }
+                        }
+                    }
+                    else if (count > 43) {
+                        // Normal calculation once the first 43 samples have been obtained
+                        avgE = 0;
+                        for (int i = 0; i < E.length; i++) {
+                            avgE += E[i];
+                        }
+                        avgE /= E.length;
+                        // Calculate C value
+                        /*
+                        V = 0;
+                        for (int i = 0; i < E.length; i++){
+                            V += E[i] - avgE;
+                        }
+                        V /= E.length;
+                        C = -0.0025714 * V + 1.5142875;
+                        */
                         // Compare instant and average energy to detect a beat
                         if (e > C * avgE) {
                             beats.add(new BeatInfo((double)position / 44100 / 4, 1));
@@ -95,11 +130,28 @@ public class AudioProcessor {
                     bytesBuffer = new byte[bufferSize * 2];
                     position += wavStream.read(bytesBuffer);
                 }
+                // Clean up beat data
+                finalBeatList = new ArrayList<BeatInfo>(beats.size() / 3);
+                for (int i = 0; i < beats.size(); i++){
+                    if (i == 0)
+                        finalBeatList.add(beats.get(i));
+                    else {
+                        BeatInfo nextBeat = beats.get(i);
+                        BeatInfo lastBeat = finalBeatList.get(finalBeatList.size() - 1);
+                        if (Math.abs(lastBeat.getTime() - nextBeat.getTime()) < 0.2 * lastBeat.getTime()) {
+                            nextBeat = new BeatInfo((nextBeat.getTime() + lastBeat.getTime()) / 2, (nextBeat.getSalience() + lastBeat.getSalience()) / 2);
+                            finalBeatList.set(finalBeatList.size() - 1, nextBeat);
+                        }
+                        else
+                            finalBeatList.add(nextBeat);
+
+                    }
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return beats;
+        return finalBeatList;
     }
 
 }
